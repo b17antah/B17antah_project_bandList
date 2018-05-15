a153969cd40926449060d7a3e5095a4b802ba30e
b17antah <b17antah@student.his.se>
2018-05-15 08:29:20 +0200

    JsonList in progress

--- app/src/main/java/com/example/b17antah/b17antah_project_bandlist/MainActivity.java
+++ app/src/main/java/com/example/b17antah/b17antah_project_bandlist/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.b17antah.b17antah_project_bandlist;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Bands> listBands = new ArrayList<Bands>();
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +39,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        adapter = new ArrayAdapter(getApplicationContext(), R.layout.textview_list,
+                R.id.item_textview, listBands);
+
+        ListView myListView = (ListView) findViewById(R.id.my_listView);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Bands des = (Bands) adapter.getItem(position);
+
+                Toast.makeText(getApplicationContext(), des.bandInfo(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        new FetchData().execute();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,5 +81,100 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class FetchData extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            // These two variables need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a Java string.
+            String jsonStr = null;
+
+            try {
+                // Construct the URL for the Internet service
+                URL url = new URL("http://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?login=b17antah");
+
+                // Create the request to the PHP-service, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                jsonStr = buffer.toString();
+                return jsonStr;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in
+                // attempting to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("Network error", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String o) {
+            super.onPostExecute(o);
+            Log.d("kot", "DataFetched:" + o);
+
+            try {
+
+                // När vi har ett JSONObjekt kan vi hämta ut dess beståndsdelar
+                JSONArray a = new JSONArray(o);
+
+                for (int each = 0; each < a.length(); each++) {
+
+                    JSONObject getBands = a.getJSONObject(each);
+                    String getName = getBands.getString("Name");
+                    String getWhere = getBands.getString("Location");
+                    String getType = getBands.getString("Company");
+                    int getWhen = getBands.getInt("Cost");
+
+                    Bands showBands = new Bands(getName, getWhere, getType, getWhen);
+                    adapter.add(showBands);
+                }
+            } catch (JSONException e) {
+                Log.e("brom", "E:" + e.getMessage());
+
+            }
+            // This code executes after we have received our data. The String object o holds
+            // the un-parsed JSON string or is null if we had an IOException during the fetch.
+
+            // Implement a parsing code that loops through the entire JSON and creates objects
+            // of our newly created com.example.brom.listviewjsonapp.Mountain class.
+        }
     }
 }
